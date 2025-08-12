ad91293e0dc0211e0f219aa345a28c851e950fe4
Babak <babak@Babaks-MacBook-Pro.local>
2025-08-12 10:20:40 -0400

    test

--- agent/agent_distributed.py
+++ agent/agent_distributed.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Defines the D4PG agent class."""
+"""Defines the D4PG agent class"""
 
 import copy
 from typing import Callable, Dict, List, Optional
